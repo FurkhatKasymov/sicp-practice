5ea933838156156d4bea6e2358fc7d3886f67efc
furkhat <vailodf@gmail.com>
2016-03-25 23:39:19 +0600

    right-split and up-split using general split procedure

---
+++ 2.2.4-example-a-picture-language/split.scm
@@ -0,0 +1,20 @@
+#lang scheme
+
+(require sicp-pict)
+
+(define (split p1 p2)
+  (define (proc painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (proc painter (- n 1))))
+          (p1 painter (p2 smaller smaller)))))
+  proc)
+
+(define right-split
+  (split beside below))
+
+(define up-split
+  (split below beside))
+
+(paint (right-split einstein 3))
+(paint (up-split einstein 3))
