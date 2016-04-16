b8b5177a53bfd7a97e92f2c3648ff045a3c85d69
furkhat <vailodf@gmail.com>
2016-04-16 17:57:11 +0600

    added linear pascal traingle

--- 1.2.2-tree-recursion/pascal-traingle.scm
+++ 1.2.2-tree-recursion/pascal-traingle.scm
@@ -8,3 +8,17 @@
           (+ 
             (pascal-triangle-recursive (- r 1) d) 
             (pascal-triangle-recursive (- r 1) (- d 1))))))
+
+; pascal's triangle actually captures the idea of how many different
+; ways there are of chosing objects from a set, where the order of choice doesn't matter.
+
+(define (fact n)
+  (define (fact-iter x res)
+    (if (= x 0)
+      res
+      (fact-iter (- x 1) (* res x))))
+  (fact-iter n 1))
+
+(define (pascal-traingle-linear r d)
+  (/ (fact r)
+     (* (fact (- r d)) (fact d))))
