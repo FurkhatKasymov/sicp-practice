dc9072ceabb559cd95e2ad97a8320a7f4b93f808
furkhat <vailodf@gmail.com>
2016-05-27 16:44:27 +0600

    apply generic multiple args exercise 2.82

---
+++ 2.5-system-with-generic-operations/apply-generic-multiple-arguments-coercion.scm
@@ -0,0 +1,32 @@
+(define (can-be-coerced? type-tags target-type)
+  (if (null? type-tags)
+      true
+      (and (or (equal? (car type-tags) target-type)
+               (get-coercion (car type-tags) target-type))
+           (can-be-coerced? (cdr type-tags) target-type))))
+
+
+(define (get-type-coerce-to type-tags)
+  (define (iter-proc current-type rest)
+    (if (can-be-coerced? current-type type-tags)
+        current-type
+        (if (null? rest)
+            false
+            (iter-proc (car rest) (cdr rest))))))
+
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (let ((coerce-to-type (get-type-coerce-to type-tags)))
+              (if coerce-to-type
+                (apply-generic op 
+                               (map (lambda (x) 
+                                      (if (equal? (type-tag x) coerce-to-type)
+                                          x
+                                          ((get-coercion (type-tag x) coerce-to-type) x)))
+                                    args))
+                (error "No method for these types"
+                  (list op type-tags))))))))
